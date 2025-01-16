88f61928abebbd6327730884cef8d66ba2a62b60
makpalu <mawunyolitsky@gmail.com>
2025-01-15 16:28:41 -0800

    Commiting new chatbot update

--- chatbot.py
+++ chatbot.py
@@ -2,8 +2,14 @@
 
 # Purpose is to be a friend bot
 
-questions = ["hello", "how are you", "what do you like to do for fun",
-             "what do you like most about yourself", "where do you come from", "what should i name you"]
+import difflib
+
+questions = [["hello", "hey", "hi", "sup","howdy"],
+             ["how are you", "How do you do", "how are you feeling","how is life"],
+             ["what do you like to do for fun", "what do you do in your free time"],
+             ["what do you like most about yourself", "what makes you, you"],
+             ["where do you come from", "where do you live"],
+             ["what should i name you", "what would you like to be called", "what is your name"]]
 
 answers = ["Hey! Nice to meet you 😀", "I am doing splendid 😊", "It depends on where I am but mostly programming 🙃",
            "The fact that I can do what I want 😁", "Wherever you'd like me to be from 😉", "Your best friend 😜"]
@@ -20,11 +26,17 @@ while True:
         print("Goodbye! Nice chatting with you")
         break
 
-    for index, question in enumerate(questions):
-        if user_input == question:
-            print(answers[index])
+    best_match = None
+    best_index = -1
+
+    for index, variations in enumerate(questions):
+        match = difflib.get_close_matches(user_input, variations, n=1, cutoff=0.6)
+
+        if match:
+            best_match = match[0]
+            best_index = index
             break
+    if best_match:
+        print(answers[best_index])
     else:
         print("Question not recognised.")
-
-
